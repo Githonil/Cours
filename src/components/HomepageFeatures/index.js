1779aa6f2a1124efc6ec26c1a244483f64a1b29c
Githonil <anthony.gimenez06@gmail.com>
2024-09-25 18:23:22 +0200

    Upgrade button

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,39 +1,46 @@
-import clsx from "clsx";
-import Heading from '@theme/Heading';
+import React from 'react';
 import styles from './styles.module.css';
+import { useColorMode } from '@docusaurus/theme-common'
 import Link from "@docusaurus/Link";
 
 const links = [
     {
         id: 'algo',
         title: 'Algorithmique',
+        Svg: require('@site/static/img/algo-ico.svg').default,
         link: '/docs/category/algorithmique/'
     },
     {
         id: 'math',
         title: 'Mathématiques',
+        Svg: require('@site/static/img/math-ico.svg').default,
         link: '/docs/category/mathématiques/'
     },
     {
         id: 'os',
         title: 'Architecture et OS',
+        Svg: require('@site/static/img/archi-ico.svg').default,
         link: '/docs/category/architecture-et-os/'
     },
     {
         id: 'c',
         title: 'Langage C',
+        Svg: require('@site/static/img/prog-ico.svg').default,
         link: '/docs/category/langage-c/'
     },
     {
         id: 'anglais',
         title: 'Anglais',
+        Svg: require('@site/static/img/en-ico.svg').default,
         link: '/docs/category/anglais/'
     },
 ];
 
-function Button({id, title, link}) {
+function Button({id, title, link, Svg}) {
     return (
-        <Link className={styles.linkPage} to={link}>{title}</Link>
+        <Link className={styles.linkPage} to={link}>
+            <Svg id={id} role="img" className={styles.imgButton}/>
+      {title}</Link>
     );
 }
 
