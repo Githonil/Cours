498982e4d266964439e5dae71008bef3201f0fa2
Githonil <anthony.gimenez06@gmail.com>
2024-09-26 15:23:41 +0200

    Changement du responsive et ajout d'un titre sur la page d'accueil

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import Buttons from '../components/Buttons';
+import Heading from '@theme/Heading'
 
 import styles from './index.module.css';
 
@@ -10,6 +11,14 @@ export default function Home() {
   return (
     <Layout
       description="Description will go into a meta tag in <head />">
+      <header className={clsx(styles.title, styles.heroBanner)}>
+        <Heading as="h1" className="hero_title">
+          Polytech Tours
+        </Heading>
+        <Heading as="h2" className='hero_title'>
+           Répertoire des cours
+        </Heading>
+      </header>
       <main className={clsx("hero hero--primary", styles.main)}>
         <Buttons />
       </main>
